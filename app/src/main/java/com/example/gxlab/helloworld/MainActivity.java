c08551b28399cd8e752cd05491b7c950ecb2bd71
Xuan Do <doxuancntp@gmail.com>
2018-05-15 16:09:59 -0400

    Fixed the scrolling menu.

--- app/src/main/java/com/example/gxlab/helloworld/MainActivity.java
+++ app/src/main/java/com/example/gxlab/helloworld/MainActivity.java
@@ -4,6 +4,9 @@ import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
+import com.google.android.glass.view.WindowUtils;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 import android.app.Activity;
 import android.content.Context;
@@ -13,16 +16,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
-
-import com.google.android.glass.view.WindowUtils;
-
-
-
 import android.view.Window;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.GestureDetector;
 import android.view.WindowManager;
+import android.view.MotionEvent;
+
 
 /**
  * An {@link Activity} showing a tuggable "Hello World!" card.
@@ -95,11 +94,67 @@ public class MainActivity extends Activity {
                 // Plays disallowed sound to indicate that TAP actions are not supported.
                 AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
                 am.playSoundEffect(Sounds.DISALLOWED);
+
+                openOptionsMenu();
+            }
+
+
+        });
+
+        mGestureDetector = createGestureDetector(this);
+        setContentView(mCardScroller);
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.TAP) {
+                    openOptionsMenu();
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    // do something on right (forward) swipe
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    // do something on left (backwards) swipe
+                    return true;
+                } else if (gesture == Gesture.SWIPE_DOWN){
+                    finish();
+                }
+                return false;
             }
         });
 
-//        mGestureDetector = createGestureDetector(this);
-        setContentView(mCardScroller);
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+                // do something on finger count changes
+            }
+        });
+
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                // do something on scrolling
+                return true;
+            }
+        });
+
+        return gestureDetector;
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 
 
